189bc93f045e87924de3b5e44b973ff1122ed41b
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-09-08 09:40:00 -0400

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,1 +1,15 @@
+from typing import Optional
+
+import numpy as np
+from pennylane import AngleEmbedding
 from sklearn.utils.estimator_checks import check_estimator
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.multiclass import unique_labels
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.metrics.pairwise import pairwise_kernels
+import pennylane as qml
+import pythonbasictools as pbt
+
+
+class SVMFromScratch(BaseEstimator):
+    raise NotImplementedError()
