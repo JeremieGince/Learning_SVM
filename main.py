4ca4be36f57db93ada491cb92418e65df7bdaebd
Jérémie Gince <50332514+JeremieGince@users.noreply.github.com>
2023-09-09 22:39:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,22 @@ import numpy as np
 import pandas as pd
 from sklearn import datasets
 from sklearn import svm
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 from kernels import ClassicalKernel, QuantumKernel
 from scratch import SVMFromScratch
 from visualization import Visualizer
 
 if __name__ == '__main__':
-    n_qubits = 4
+    n_qubits = 2
     # dataset = datasets.load_breast_cancer(as_frame=True)
     # dataset = datasets.load_iris(as_frame=True)
     dataset = datasets.make_classification(
         n_samples=100,
-        n_features=4,
+        n_features=2,
         n_classes=2,
         n_clusters_per_class=1,
-        n_informative=4,
+        n_informative=2,
         n_redundant=0,
         random_state=0,
     )
@@ -35,7 +35,8 @@ if __name__ == '__main__':
     else:
         raise ValueError(f"Unknown dataset type: {type(dataset)}")
 
-    X = StandardScaler().fit_transform(X)
+    # X = StandardScaler().fit_transform(X)
+    X = MinMaxScaler(feature_range=(0, 1)).fit_transform(X)
     # y = MinMaxScaler(feature_range=(-1, 1)).fit_transform(y.reshape(-1, 1)).reshape(-1).astype(int)
     print(f"{X.shape = }, {y.shape = }")
     print(f"{np.unique(y) = }")
@@ -57,15 +58,16 @@ if __name__ == '__main__':
 
     clas_model = svm.SVC(kernel=clas_kernel.kernel, random_state=0)
     qml_model = svm.SVC(kernel=q_kernel.kernel, random_state=0)
-    scratch_model = SVMFromScratch(kernel="rbf")
+    scratch_model = SVMFromScratch(kernel="rbf", max_iter=1_000)
     
     models = {
-        "classical": clas_model,
+        # "classical": clas_model,
         # "qml": qml_model,
         "scratch": scratch_model,
     }
 
     fig, axes = plt.subplots(len(models), 1, tight_layout=True, figsize=(14, 10), sharex="all", sharey="all")
+    axes = np.ravel(np.asarray([axes]))
     for i, (m_name, model) in enumerate(models.items()):
         fit_start_time = time.time()
         model.fit(X, y)
@@ -89,3 +91,5 @@ if __name__ == '__main__':
         ax.set_title(f"{m_name} accuracy: {accuracy * 100:.2f}%")
 
     plt.show()
+
+    models["scratch"].visualize(X, y)
